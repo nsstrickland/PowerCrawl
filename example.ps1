6d14e0c2b66391872f00c225c33dc750aeeefc0c
Nick Strickland <nsstrickland@outlook.com>
2021-12-24 01:36:07 -0500

    update before trip

--- example.ps1
+++ example.ps1
@@ -8,63 +8,65 @@
 # .DESCRIPTION: PowerCrawl
 #>
 
-#"Creature" section 
- #Start off with a basis for `all` creatures: their statistics
+#Statistic Base Class
+#TODO: Determine how skills fit into the mix
 class Stat {
     [string]$Name;
     [int]$Value;
-    [int]$Modifier=[System.Math]::Round(($Value-10)/2);
+    [int]$Modifier = [System.Math]::Round(($Value - 10) / 2);
+
     Stat (
         [string]$Name,
         [int]$Value
     ) {
-        $this.Name=$Name;
-        $this.Value=$Value
+        $this.Name = $Name;
+        $this.Value = $Value
     }
     [string] ToString() {
-        return ($this.Name+'='+$this.Value+' [ '+$this.Modifier+' ];')
-    }
-    
-
+        if ( $this.Modifier -gt -1 ) {
+            $retMod = [string]'+' + $this.Modifier
+        }
+        else {
+            $retMod = [string]$this.Modifier
+        }
+        return ($this.Name + ' = ' + $this.Value + ' (' + $retMod + ');')
     }
 
 }
- class StatTable {
-    [int]$Strength;
-    [int]$Vitality;
-    [int]$Experience;
+class StatBlock {
+    [Stat[]]$AbilityScores;
     [int]$Level;
-    hidden [int]$LevelIncrement;
-    StatTable() { #constructor if we want the defaults
-        $this.Strength=10
-        $this.Vitality=5;
-        $this.Experience = 0;
-        $this.Level = 1;
-        $this.LevelIncrement = 50;
-    }
-    StatTable( #constructor if we don't want the defaults
+    [int]$Experience;
+    [int]$LevelIncrement; #The experience total at which the character will level up; TODO: Incorporate location?
+    StatBlock(
         [int]$Strength,
-        [int]$Vitality,
+        [int]$Dexterity,
+        [int]$Constitution,
+        [int]$Intelligence,
+        [int]$Wisdom,
+        [int]$Charisma,
         [int]$Level
-    ){
-        $this.Strength = $Strength;
-        $this.Vitality = $Vitality;
+    ) {
+        $this.AbilityScores = [Stat[]]@([Stat]::new("Strength",$Strength), [Stat]::new("Dexterity",$Dexterity), [Stat]::new("Constitution",$Constitution), [Stat]::new("Intelligence",$Intelligence), [Stat]::new("Wisdom", $Wisdom), [Stat]::new("Charisma",$Charisma))
         $this.Level = $Level
-        $this.LevelIncrement = 50;
-        $this.Experience = (($level*$this.LevelIncrement) - $this.LevelIncrement);
+        #temp formula plugins
+        [int]$MonsterXP=45 #Average experience granted by base monster, TBD by location
+        [int]$Difficulty=1 #Difficulty factor (reference model starts an exponential increase at ~30), leaving at 1 for now
+        [int]$ReductionFactor=1 #Difficulty factor reduction, completely TBD, needs to be fine-tuned
+        $this.LevelIncrement = (((8 * $Level) + ($Difficulty * $Level)) * ($MonsterXP * $Level) * ($ReductionFactor * $Level))
     }
-    [string]ToString(){
-        return ("str=" + $this.Strength + ", vit=" + $this.Vitality + ", lvl=" +$this.Level)
-    }
-    [bool]AddExp($XpToAdd){ #returns true or false to tell if we leveled up or not
-        $this.Experience+=$XpToAdd;
-        if ( $this.Experience -ge ($this.Level*$this.LevelIncrement)) {
+    [string]ToString() {return "test"}
+    [bool]AddExp($XpToAdd) {
+        #returns true or false to tell if we leveled up or not
+        $this.Experience += $XpToAdd;
+        if ( $this.Experience -ge ($this.Level * $this.LevelIncrement)) {
             $this.Level++;
-            $this.Strength+=2;
+            $this.Strength += 2;
             $this.Vitality++;
             Write-Host -ForegroundColor Green "You leveled up!"
             return $true;
-        } else {
+        }
+        else {
             return $false;
         }
     }
@@ -83,24 +85,26 @@ class Creature {
         [int]$Level
     ) {
         $this.Name = $Name;
-        $this.Stats = [StatTable]::new($Strenth,$Vitality,$Level)
-        $this.Health = ($this.Stats.Vitality/2)
+        $this.Stats = [StatTable]::new($Strenth, $Vitality, $Level)
+        $this.Health = ($this.Stats.Vitality / 2)
     }
     
-    [void]EquipItem([Weapon]$ItemToAdd) { #method to equip an item
+    [void]EquipItem([Weapon]$ItemToAdd) {
+        #method to equip an item
         $this.EquippedWeapon = $ItemToAdd;
     }
     [void]UnEquipItem() {
         $this.EquippedWeapon = $null
     }
-    [ActionEvent]Attack([Creature]$target){ #method to initiate an attack, will equip "fist" item if there's no weapon equipped
+    [ActionEvent]Attack([Creature]$target) {
+        #method to initiate an attack, will equip "fist" item if there's no weapon equipped
         if (!$this.EquippedWeapon) {
-            $this.EquippedWeapon = [Weapon]::new("Fist",5,2)
+            $this.EquippedWeapon = [Weapon]::new("Fist", 5, 2)
         }
-        return [ActionEvent]::new($this,$target,$this.EquippedWeapon)
+        return [ActionEvent]::new($this, $target, $this.EquippedWeapon)
     }
-    [string]ToString(){
-        return ($this.Name + "hp:"+$this.Health)
+    [string]ToString() {
+        return ($this.Name + "hp:" + $this.Health)
     }
 }
 #Event for attacks-- it will act on both the target and the source, modifying health and exp
@@ -115,12 +119,12 @@ class ActionEvent {
         [Weapon]$Weapon) {
         $this.Source = $Source;
         $this.Target = $Target;
-        $this.DamageDealt = Get-Random -Minimum ($Weapon.Damage/2) -Maximum $Weapon.Damage;
+        $this.DamageDealt = Get-Random -Minimum ($Weapon.Damage / 2) -Maximum $Weapon.Damage;
 
-        $this.Source.Stats.AddExp($this.DamageDealt/2);
-        $this.Target.Health-=$this.DamageDealt;
+        $this.Source.Stats.AddExp($this.DamageDealt / 2);
+        $this.Target.Health -= $this.DamageDealt;
         if ( $this.Target.Health -le 0 ) {
-            Write-Host -ForegroundColor Red -Object ($this.Target.Name+" has died!");
+            Write-Host -ForegroundColor Red -Object ($this.Target.Name + " has died!");
             $this.Source.Stats.AddExp(10)
         }
     }
@@ -130,11 +134,11 @@ class Weapon {
     [int]$Durability;
     [int]$MaxDurability;
     [int]$Damage
-    Weapon ($Name,$Damage,$MaxDurability) {
+    Weapon ($Name, $Damage, $MaxDurability) {
         $this.Name = $Name;
         $this.Damage = $Damage
-        $this.Durability=$MaxDurability;
-        $this.MaxDurability=$MaxDurability;
+        $this.Durability = $MaxDurability;
+        $this.MaxDurability = $MaxDurability;
     }
 }
 
@@ -142,4 +146,4 @@ class Weapon {
 $obj=[Creature]::new("Player",12,10,1)
 $atk = [Creature]::new("Monster",12,10,1)
 $obj.Attack($atk)
-#>         
+#>
